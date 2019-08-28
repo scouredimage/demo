f36e4d2e6f8b9d3f7029c31798c53a9e02baa758
Sandesh Devaraju <scouredimage@gmail.com>
2019-08-27 22:29:36 -0700

    init

---
+++ handler.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { promisify } = require('util')
+const exec = promisify(require('child_process').exec)
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function usage() {
+  const { stdout } = await exec("cat /proc/meminfo")
+  let memInfoSplits = stdout.split(/[\n: ]/).filter(val => val.trim())
+  const available = parseInt(memInfoSplits[1])
+  const used =  parseInt(memInfoSplits[19])
+  return used / available * 100
+}
+
+var arr = []
+async function allocate(percent) {
+  var i = 0
+  while (i < 3) {
+    i++
+    const used = await usage()
+    if (used >= percent) {
+      break
+    }
+    arr.push({buff: Buffer.alloc(1024 * 1024)})
+  }
+}
+
+module.exports.hello = async (event, context) => {
+  //try {
+  //  throw new Error('i died')
+  //} catch (error) {
+  //  context.captureError(error)
+  //}
+  //throw new Error('i died')
+  //await sleep(3000)
+
+  await allocate(60)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+};
