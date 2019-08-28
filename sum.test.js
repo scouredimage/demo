69d654c26962f8e198b415542b60d6e38fea13d2
astuyve <astuyve@gmail.com>
2019-08-28 11:29:38 -0500

    YOLO THIS STUFF TO PROD

---
+++ sum.test.js
@@ -0,0 +1,11 @@
+describe('add', () =>{
+  it('adds stuff', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
+
+describe('sum', () =>{
+  it('adds stuff', () => {
+    expect(sum(1, 1)).toBe(2)
+  })
+})
