4bf3b70b7c3cc66f670818b5df2388dc7aeb8e14
Sandesh Devaraju <scouredimage@gmail.com>
2019-09-07 15:32:10 -0700

    pr summary

--- handler.js
+++ handler.js
@@ -43,7 +43,7 @@ module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'hi summary preview builds!',
+      message: 'hi summary pr builds!',
       input: event,
     }),
   };
